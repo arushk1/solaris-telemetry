ed7beb868d8bfb0a58d36c31da95d4b65b3c0168
Arush <arushk1@gmail.com>
2014-12-24 22:23:14 +0000

    Remove CV2

--- Helios.py
+++ Helios.py
@@ -7,8 +7,6 @@ import RPi.GPIO as GPIO
 from random import randint
 from gps import *
 import threading
-import numpy as np
-import cv2
 
 mgui = Tk()
 mgui.geometry("600x300")
@@ -261,42 +259,39 @@ class GpsController(threading.Thread):
     def satellites(self):
         return self.gpsd.satellites
 
-
-
-
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-	if ((adcnum > 7) or (adcnum < 0)):
-		return -1
-	GPIO.output(cspin, True)
+    if ((adcnum > 7) or (adcnum < 0)):
+        return -1
+    GPIO.output(cspin, True)
 
-	GPIO.output(clockpin, False)  # start clock low
-	GPIO.output(cspin, False)     # bring CS low
+    GPIO.output(clockpin, False)  # start clock low
+    GPIO.output(cspin, False)     # bring CS low
 
-	commandout = adcnum
-	commandout |= 0x18  # start bit + single-ended bit
-	commandout <<= 3    # we only need to send 5 bits here
-	for i in range(5):
-		if (commandout & 0x80):
-			GPIO.output(mosipin, True)
-		else:
-   			GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
+    commandout = adcnum
+    commandout |= 0x18  # start bit + single-ended bit
+    commandout <<= 3    # we only need to send 5 bits here
+    for i in range(5):
+        if (commandout & 0x80):
+            GPIO.output(mosipin, True)
+        else:
+            GPIO.output(mosipin, False)
+        commandout <<= 1
+        GPIO.output(clockpin, True)
+        GPIO.output(clockpin, False)
 
-	adcout = 0
-	# read in one empty bit, one null bit and 10 ADC bits
-	for i in range(12):
-		GPIO.output(clockpin, True)
-		GPIO.output(clockpin, False)
-		adcout <<= 1
-		if (GPIO.input(misopin)):
-			adcout |= 0x1
+    adcout = 0
+    # read in one empty bit, one null bit and 10 ADC bits
+    for i in range(12):
+        GPIO.output(clockpin, True)
+        GPIO.output(clockpin, False)
+        adcout <<= 1
+        if (GPIO.input(misopin)):
+            adcout |= 0x1
 
-	GPIO.output(cspin, True)
+    GPIO.output(cspin, True)
 
-	adcout /= 2       # first bit is 'null' so drop it
-	return adcout
+    adcout /= 2       # first bit is 'null' so drop it
+    return adcout
 
 
         
